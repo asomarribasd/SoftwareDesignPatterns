95303d533f8a3da1a87746bb4372aacee8cc6614
Ahmed Somarribas <asomarribasd@gmail.com>
2015-09-17 23:53:36 -0600

    Add  Builder Base
    Add  Builder Base

---
+++ CSharpDesignPatterns/DesignPatterns/GOF/Builder.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.GOF
+{
+    /// <summary>
+    /// The 'Builder' abstract class
+    /// </summary>
+    public abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract Product GetProductCreated();
+    }
+
+    /// <summary>
+    /// The 'Director' class
+    /// </summary>
+    public class Director
+    {
+        // Builder uses a complex series of steps
+        public void Construct(Builder builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteBuilder1' class
+    /// </summary>
+    public class ConcreteBuilder1 : Builder
+    {
+        private Product _product = new Product();
+
+        public override void BuildPartA()
+        {
+            _product.Add("PartA");
+        }
+
+        public override void BuildPartB()
+        {
+            _product.Add("PartB");
+        }
+
+        public override Product GetProductCreated()
+        {
+            return _product;
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteBuilder2' class
+    /// </summary>
+    public class ConcreteBuilder2 : Builder
+    {
+        private Product _product = new Product();
+
+        public override void BuildPartA()
+        {
+            _product.Add("PartX");
+        }
+
+        public override void BuildPartB()
+        {
+            _product.Add("PartY");
+        }
+
+        public override Product GetProductCreated()
+        {
+            return _product;
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' class
+    /// </summary>
+    public class Product
+    {
+        private List<string> _parts = new List<string>();
+
+        public void Add(string part)
+        {
+            _parts.Add(part);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\nProduct Parts -------");
+            foreach (string part in _parts)
+                Console.WriteLine(part);
+        }
+    }
+
+}
---
+++ CSharpDesignPatterns/DesignPatternsTest/BuilderTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DesignPatterns.GOF;
+
+namespace DesignPatternsTest
+{
+    [TestClass]
+    public class BuilderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Director director = new Director();
+
+            Builder b1 = new ConcreteBuilder1();
+            Builder b2 = new ConcreteBuilder2();
+
+            // Construct two products
+            director.Construct(b1);
+            Product p1 = b1.GetProductCreated();
+            p1.Show();
+
+            director.Construct(b2);
+            Product p2 = b2.GetProductCreated();
+            p2.Show();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
