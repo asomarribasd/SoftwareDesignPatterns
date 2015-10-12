0e39843a5fdfb9ed1f74ad8e2aa45a34e7f2868e
Ahmed Somarribas <asomarribasd@gmail.com>
2015-10-11 23:36:58 -0600

    Decorator

--- CSharpDesignPatterns/DesignPatterns/GOF/Structural/Decorator.cs
+++ CSharpDesignPatterns/DesignPatterns/GOF/Structural/Decorator.cs
@@ -6,7 +6,77 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns.GOF.Structural
 {
-    public class Decorator
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+    abstract class Component
     {
+        public abstract void Operation();
     }
+
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+    class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent.Operation()");
+        }
+    }
+
+
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+    abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecoratorA' class
+    /// </summary>
+    class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecoratorB' class
+    /// </summary>
+    class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        void AddedBehavior()
+        {
+        }
+    }
+
+
+
+
 }
